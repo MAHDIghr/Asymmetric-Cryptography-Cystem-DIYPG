88729293ded20cf77fd4abb40fb43d635e2b1a4f
NOTuglyduckling <osearle65@gmail.com>
2025-03-28 17:12:30 +0100

    Refactor

--- Moodle RSA/Moodle RSA/phase1.c
+++ Moodle RSA/Moodle RSA/phase1.c
@@ -23,68 +23,44 @@ void rsa_decrypt(uint8_t *input, uint64_t length, uint8_t *output, rsaKey_t *pri
     }
 }
 
-void test_rsa_encrypt_decrypt() {
+void test_rsa(const uint8_t *input, uint64_t length, const char *test_name) {
+    /// \brief Fonction générique de test du chiffrement et du déchiffrement
     rsaKey_t pubKey, privKey;
     genKeysRabin(&pubKey, &privKey, 257);
     
-    uint8_t input[] = "HELLO WORLD";
-    uint64_t length = strlen((char *)input);
     uint8_t encrypted[length];
     uint8_t decrypted[length];
     
-    rsa_encrypt(input, length, encrypted, &pubKey);
+    rsa_encrypt((uint8_t *)input, length, encrypted, &pubKey);
     rsa_decrypt(encrypted, length, decrypted, &privKey);
     
     assert(memcmp(input, decrypted, length) == 0);
-    printf("Test de base réussi.\n");
+    printf("%s réussi.\n", test_name);
+}
+
+void test_rsa_encrypt_decrypt() {
+    uint8_t input[] = "HELLO WORLD";
+    test_rsa(input, strlen((char *)input), "Test de base");
 }
 
 void test_rsa_special_characters() {
-    rsaKey_t pubKey, privKey;
-    genKeysRabin(&pubKey, &privKey, 257);
-    
     uint8_t input[] = "!@#$%^&*()";
-    uint64_t length = strlen((char *)input);
-    uint8_t encrypted[length];
-    uint8_t decrypted[length];
-    
-    rsa_encrypt(input, length, encrypted, &pubKey);
-    rsa_decrypt(encrypted, length, decrypted, &privKey);
-    
-    assert(memcmp(input, decrypted, length) == 0);
-    printf("Test des caractères spéciaux réussi.\n");
+    test_rsa(input, strlen((char *)input), "Test des caractères spéciaux");
 }
 
 void test_rsa_binary_data() {
-    rsaKey_t pubKey, privKey;
-    genKeysRabin(&pubKey, &privKey, 257);
-    
     uint8_t input[] = {0x00, 0xFF, 0x7F, 0x80};
-    uint64_t length = sizeof(input);
-    uint8_t encrypted[length];
-    uint8_t decrypted[length];
-    
-    rsa_encrypt(input, length, encrypted, &pubKey);
-    rsa_decrypt(encrypted, length, decrypted, &privKey);
-    
-    assert(memcmp(input, decrypted, length) == 0);
-    printf("Test des données binaires réussi.\n");
+    test_rsa(input, sizeof(input), "Test des données binaires");
 }
 
 void test_rsa_empty_input() {
-    rsaKey_t pubKey, privKey;
-    genKeysRabin(&pubKey, &privKey, 257);
-    
     uint8_t input[] = "";
-    uint64_t length = strlen((char *)input);
-    uint8_t encrypted[1];
-    uint8_t decrypted[1];
-    
-    rsa_encrypt(input, length, encrypted, &pubKey);
-    rsa_decrypt(encrypted, length, decrypted, &privKey);
-    
-    assert(length == 0);
-    printf("Test avec entrée vide réussi.\n");
+    test_rsa(input, strlen((char *)input), "Test avec entrée vide");
+}
+
+void test_rsa_extended() {
+    uint8_t input[] = "Ceci est un test plus long avec plusieurs caractères.";
+    test_rsa(input, strlen((char *)input), "Test avec une phrase complète");
 }
 
 int main() {
@@ -92,6 +68,7 @@ int main() {
     test_rsa_special_characters();
     test_rsa_binary_data();
     test_rsa_empty_input();
-    printf("Tout les tests phase 1.1 passés\n");
+    test_rsa_extended();
+    printf("Tous les tests phase 1.1 réussis.\n");
     return 0;
 }
